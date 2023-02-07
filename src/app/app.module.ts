0ce9852255be26d4e19e93e49fd2edaaabefac88
Seun Oshitade <seun.oshitade@gmail.com>
2023-02-07 00:04:19 -0700

    Generate product-view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductViewComponent } from './products/product-view/product-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductViewComponent
   ],
   imports: [
     BrowserModule,
